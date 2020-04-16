8380641cb06d843023eb7a37c9c61827540f164a
ShunOmine <shun12baseball@gmail.com>
2020-04-16 09:22:29 +0900

    new: initialize react app with webpack

---
+++ README.md
@@ -0,0 +1,12 @@
+# ReactContextToDoApp
+react contextを使用して作成するtodoアプリです。
+
+## Install
+```
+$ yarn install
+```
+
+## Build && Server
+```
+$ yarn run watch
+```
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+require('@babel/register');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  mode: 'development',
+  entry: src + '/index.tsx',
+
+  output: {
+    path: dist,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    modules: [src, "node_modules"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: src + '/index.html',
+      filename: 'index.html'
+    })
+  ]
+}
